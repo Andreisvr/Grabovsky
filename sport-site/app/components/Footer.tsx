50873128c949295861e77569c426c658c57d12c2
Andreisvr <118602341+Andreisvr@users.noreply.github.com>
2025-10-28 16:06:50 +0200

    Delete sport-site directory

--- sport-site/app/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="section text-center text-gray-500 text-sm border-t border-gray-700">
-        <p>© {new Date().getFullYear()} SVR DEV. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
